23005913ed13547157e5733c478a64ab9e7a9704
ilia0312 <stashkov.ilia0312@yandex.ru>
2022-12-27 17:59:44 +0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " +age+ ", то он совершеннолетний");
+        }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " +age+ ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+        int age2 = 31;
+        if (age2 >= 18) {
+            System.out.println("Если возраст человека равен " +age2+ ", то он совершеннолетний");
+        }
+        if (age2 < 18) {
+            System.out.println("Если возраст человека равен " +age2+ ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+        int temperature = -10;
+        if (temperature >= 5) {
+            System.out.println("На улице " +temperature+ " градусов, можно идти без шапки");
+        }
+        else {
+            System.out.println("На улице " +temperature+ " градусов, нужно надеть шапку");
+        }
+
+        int temperature2 = 17;
+        if (temperature2 >= 5) {
+            System.out.println("На улице " +temperature2+ " градусов, можно идти без шапки");
+        }
+        else {
+            System.out.println("На улице " +temperature2+ " градусов, нужно надеть шапку");
+        }
+        }
+
+
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+        int speed = 50;
+        if (speed >= 60) {
+            System.out.println("Если скорость " +speed+ ", то придется заплатить штраф");
+        }
+        else {
+            System.out.println("Если скорость " +speed+ ", то можно ездить спокойно");
+        }
+
+        int speed2 = 80;
+        if (speed2 >= 60) {
+            System.out.println("Если скорость " +speed2+ ", то придется заплатить штраф");
+        }
+        else {
+            System.out.println("Если скорость " +speed2+ ", то можно ездить спокойно");
+        }
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+    }
+
+    public static void task6 () {
+        System.out.println("Задача 6");
+    }
+
+    public static void task7 () {
+        System.out.println("Задача 7");
+    }
+
+    public static void task8 () {
+        System.out.println("Задача 8");
+    }
+
+    public static void task9 () {
+        System.out.println("Задача 9");
+    }
+}
